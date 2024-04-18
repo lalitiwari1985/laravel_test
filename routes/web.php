618e4ed590a795cea97a3d0f3c9f33c31abc52db
lalit1985 <lalitiwari1985@gmail.com>
2024-04-18 19:00:40 +0530

    first commit

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeeController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/', [EmployeeController::class, 'index']);
+Route::post('/store', [EmployeeController::class, 'store'])->name('store');
+Route::get('/fetchall', [EmployeeController::class, 'fetchAll'])->name('fetchAll');
+Route::delete('/delete', [EmployeeController::class, 'delete'])->name('delete');
+Route::get('/edit', [EmployeeController::class, 'edit'])->name('edit');
+Route::post('/update', [EmployeeController::class, 'update'])->name('update');
